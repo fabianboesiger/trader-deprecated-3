b118089d18c29c4ccfc1f73ce355730c0fe1f60c
fabianboesiger <boesiger.fabian@outlook.com>
2020-10-27 16:01:41 +0100

    fix ema

--- src/indicators/series/ema.rs
+++ src/indicators/series/ema.rs
@@ -1,11 +1,11 @@
 use super::Series;
 
 #[derive(Clone)]
-pub struct Ema<const ALPHA: f64> {
+pub struct Ema<const PERIOD: f64> {
     ema: Option<f64>,
 }
 
-impl<const ALPHA: f64> Series for Ema<ALPHA> {
+impl<const PERIOD: f64> Series for Ema<PERIOD> {
     type Analysis = f64;
 
     fn new() -> Self {
@@ -14,7 +14,7 @@ impl<const ALPHA: f64> Series for Ema<ALPHA> {
 
     fn compute(&mut self, value: f64, recover: bool) -> Option<f64> {
         if let Some(mut ema) = self.ema.clone() {
-            ema += ALPHA * (value - ema);
+            ema += (2.0 / (PERIOD + 1.0)) * (value - ema);
             if !recover {
                 self.ema = Some(ema);
             }
