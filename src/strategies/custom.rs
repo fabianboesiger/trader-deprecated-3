7c81b7314e10174cabb70b04547b996e779c5ddf
fabianboesiger <boesiger.fabian@outlook.com>
2020-11-16 19:00:54 +0100

    strategy tweak

--- src/strategies/custom.rs
+++ src/strategies/custom.rs
@@ -37,41 +37,19 @@ impl Strategy<Indicators> for Custom {
             rsi,
             atr
         )) = analysis {
-            let bb_stdev = (upper - lower) / 2.0;
-            let bb_signal = (value - (lower + bb_stdev)) / bb_stdev;
-
-            let rsi_signal = (rsi - 50.0) / 20.0;
-
-            let signal = (bb_signal + rsi_signal) / 2.0;
-
-            println!("signal: {}, allowed to enter: {}", signal, self.allowed_to_enter);
-
-            if signal <= -1.0 && self.allowed_to_enter {
-                self.allowed_to_enter = false;
-                return Action::Enter {
-                    take_profit: Some(value + 2.0 * atr),
-                    stop_loss: Some(value - 1.9 * atr),
-                };
-            } else if signal >= 0.8 {
-                self.allowed_to_enter = true;
-                if signal >= 1.0 {
-                    return Action::Exit;
-                }
-            }
-
-
-            /*
+           
             if value > upper || rsi > 70.0 {
+                self.allowed_to_enter = true;
                 return Action::Exit;
             }
 
-            if value < lower && rsi < 30.0 && histogram > 0.0 {
+            if value < lower && rsi < 30.0 && self.allowed_to_enter {
+                self.allowed_to_enter = false;
                 return Action::Enter {
                     take_profit: Some(value + 2.0 * atr),
                     stop_loss: Some(value - 2.0 * atr),
                 };
             }
-            */
         }
 
         Action::Hold
