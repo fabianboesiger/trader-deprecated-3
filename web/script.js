f9a91a6d9d97a023623ff5d47203d180a7254740
fabianboesiger <boesiger.fabian@outlook.com>
2020-11-22 20:32:53 +0100

    changes

--- web/script.js
+++ web/script.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", event => {
-    let rateOfProfitElem = document.getElementById("rate-of-profit");
-    let lastDayRateOfProfitElem = document.getElementById("last-day-rate-of-profit");
-    let estimatedProfitsPerDayElem = document.getElementById("estimated-profits");
+    let main = document.getElementsByTagName("main");
 
     let uri = "ws://" + location.host + "/socket";
     let ws = new WebSocket(uri);
@@ -9,19 +7,6 @@ document.addEventListener("DOMContentLoaded", event => {
     ws.onopen = function() {}
 
     ws.onmessage = function(message) {
-        let data = JSON.parse(message.data);
-        if ("Trade" in data) {
-            let trade = data.Trade;
-            addTrade(trade);
-            addAsset(trade.buy.asset);
-            addAsset(trade.sell.asset);
-            updateQuantity(trade.buy.asset, trade.buy.quantity);
-            updateQuantity(trade.sell.asset, -trade.sell.quantity);
-        } else
-        if ("Value" in data) {
-            let value = data.Value;
-            addAsset(value.market.base);
-            updateValue(value.market.base, value.value);
-        }
+        main.innerHTML = message.data;
     };
 });
