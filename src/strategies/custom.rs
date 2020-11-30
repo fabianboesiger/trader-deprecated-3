1709d5595aa0d17b5aeea2625e4ed8f0781a06a1
fabianboesiger <boesiger.fabian@outlook.com>
2020-11-30 10:40:10 +0100

    new version

--- src/strategies/custom.rs
+++ src/strategies/custom.rs
@@ -2,48 +2,61 @@ use super::Strategy;
 use crate::{
     indicators::{
         series::Value,
-        Atr, BollingerBands, Current, Indicator, Rsi
+        Atr, BollingerBands, Current, Indicator, Rsi, Timestamp
     },
     model::Action,
 };
-use chrono::{DateTime, Utc, NaiveDateTime};
 use rust_decimal::prelude::*;
+use chrono::{DateTime, Utc, TimeZone, Duration};
 
 type Indicators = (
     Current<Value>,
     BollingerBands<20, 2.0>,
     Rsi<14>,
     Atr<14>,
+    Timestamp
 );
 
 #[derive(Clone)]
 pub struct Custom {
     allowed_to_enter: bool,
+    rsi_breakthrough: DateTime<Utc>,
 }
 
 impl Strategy<Indicators> for Custom {
     fn new() -> Self {
         Self {
             allowed_to_enter: false,
+            rsi_breakthrough: Utc.ymd(1970, 1, 1).and_hms_milli(0, 0, 0, 0),
         }
     }
 
     fn run(&mut self, analysis: Option<<Indicators as Indicator>::Analysis>) -> Action {
+        println!("{:?}", analysis);
+        
         if let Some((
             value,
             (upper, lower),
             rsi,
-            atr
+            atr,
+            now
         )) = analysis {
            
-            if value > upper || rsi > 70.0 {
+            if rsi > 70.0 {
                 self.allowed_to_enter = true;
                 return Action::Exit;
             }
-           
+            
+            if rsi < 30.0 {
+                self.rsi_breakthrough = now;
+            }
+            
             if 
-                value < lower &&
-                rsi < 30.0 &&
+                //rsi <= 50.0 &&
+                //value <= lower &&
+                //value < lower &&
+                rsi >= 30.0 &&
+                now - self.rsi_breakthrough <= Duration::hours(2) &&
                 //histogram >= 0.0 &&
                 1.6 * atr / value >= 0.005 && // Is it actually worth the trade?
                 self.allowed_to_enter
